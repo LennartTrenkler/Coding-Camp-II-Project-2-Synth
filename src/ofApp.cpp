db992d261df26f78f4e7d31263d234ce65e4643d
Doreen Reuchsel <doreen.reuchsel@gmail.com>
2026-02-22 18:21:22 +0100

    add draw() scaffold and waveform buffer

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,46 +3,58 @@
 #include "SquareOscillator.h"
 #include "NoiseOscillator.h"
 
-
 void ofApp::setup() {
-	ofSoundStreamSettings settings;
-	settings.setOutListener(this);
-	settings.sampleRate = 44100;
-	settings.numOutputChannels = 2;
-	settings.numInputChannels = 0;
-	settings.bufferSize = 512;
+    ofSoundStreamSettings settings;
+    settings.setOutListener(this);
+    settings.sampleRate = 44100;
+    settings.numOutputChannels = 2;
+    settings.numInputChannels = 0;
+    settings.bufferSize = 512;
 
-	soundStream.setup(settings);
-	soundStream.start();
+    soundStream.setup(settings);
+    soundStream.start();
 
-	const float sr = static_cast<float>(settings.sampleRate);
-	synth.setOscillator(std::make_unique<SineOscillator>(440.0f, sr));
+    const float sr = static_cast<float>(settings.sampleRate);
+    synth.setOscillator(std::make_unique<SineOscillator>(440.0f, sr));
+
+    waveformBuffer.resize(512, 0.0f);
 }
 
+void ofApp::update() {
+    // placeholder for now
+}
 
-void ofApp::audioOut(ofSoundBuffer & buffer) {
-	for (size_t i = 0; i < buffer.getNumFrames(); i++) {
-		float sample = synth.generateSample();
+void ofApp::draw() {
+    ofBackground(0);
+    ofSetColor(255);
+    ofDrawBitmapString("Press 1 (Sine)  2 (Square)  3 (Noise)", 20, 20);
+}
 
-		for (size_t ch = 0; ch < buffer.getNumChannels(); ch++) {
-			buffer[i * buffer.getNumChannels() + ch] = sample;
-		}
-	}
+void ofApp::audioOut(ofSoundBuffer& buffer) {
+    std::lock_guard<std::mutex> lock(bufferMutex);
+
+    for (size_t i = 0; i < buffer.getNumFrames(); i++) {
+        float sample = synth.generateSample();
+
+        waveformBuffer[i % waveformBuffer.size()] = sample;
+
+        for (size_t ch = 0; ch < buffer.getNumChannels(); ch++) {
+            buffer[i * buffer.getNumChannels() + ch] = sample;
+        }
+    }
 }
 
 void ofApp::keyPressed(int key) {
-	if (key == '1') {
-		synth.setOscillator(
-			std::make_unique<SineOscillator>(440.0f, 44100.0f));
-	}
-
-	if (key == '2') {
-		synth.setOscillator(
-			std::make_unique<SquareOscillator>(440.0f, 44100.0f));
-	}
-
-	if (key == '3') {
-		synth.setOscillator(
-			std::make_unique<NoiseOscillator>(440.0f, 44100.0f));
-	}
+    if (key == '1') {
+        oscillatorType = 1;
+        synth.setOscillator(std::make_unique<SineOscillator>(440.0f, 44100.0f));
+    }
+    if (key == '2') {
+        oscillatorType = 2;
+        synth.setOscillator(std::make_unique<SquareOscillator>(440.0f, 44100.0f));
+    }
+    if (key == '3') {
+        oscillatorType = 3;
+        synth.setOscillator(std::make_unique<NoiseOscillator>(440.0f, 44100.0f));
+    }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,14 +1,22 @@
 #pragma once
 #include "Synth.h"
 #include "ofMain.h"
+#include <vector>
+#include <mutex>
 
 class ofApp : public ofBaseApp {
 public:
-	void setup();
-	void audioOut(ofSoundBuffer & buffer);
-	void keyPressed(int key);
+    void setup();
+    void update();
+    void draw();
+    void audioOut(ofSoundBuffer& buffer);
+    void keyPressed(int key);
 
 private:
-	Synth synth;
-	ofSoundStream soundStream;
+    Synth synth;
+    ofSoundStream soundStream;
+
+    std::vector<float> waveformBuffer;
+    std::mutex bufferMutex;
+    int oscillatorType = 1;
 };
